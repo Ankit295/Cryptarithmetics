f3ba867afc171656b22f51f9b6f12f354d0e7e69
Ankit Pandey <ankit.pandey2015@vit.ac.in>
2017-09-06 13:20:03 +0530

    Signed-off-by: Ankit Pandey <ankit.pandey2015@vit.ac.in>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express=require ('express');
 var app=express();
 var path=require('path');
+app.use(express.static('ai'))
 app.get('/',function(req,res){
 
 	res.sendFile(path.join(__dirname,'Cryptarithms.html'));	
