8d8212c76c0723d9676669f20a3dd223fe350321
Ankit Pandey <ankit.pandey2015@vit.ac.in>
2017-09-06 13:08:31 +0530

    Initial
    Signed-off-by: Ankit Pandey <ankit.pandey2015@vit.ac.in>

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-var express=require('mongo');
+var express=require ('express');
+var app=express();
+var path=require('path');
+app.get('/',function(req,res){
+
+	res.sendFile(path.join(__dirname,'Cryptarithms.html'));	
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+console.log("listening at 3000");
